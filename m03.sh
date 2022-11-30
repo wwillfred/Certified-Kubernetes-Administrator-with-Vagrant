7a740ecfe5f301e5db8703f1fc00460a4824ce04
wwillfred <anicenerd+atlassian@pm.me>
2022-11-30 11:47:30 -0500

    Added code for configuring and installing containerd

--- m03.sh
+++ m03.sh
@@ -1,3 +1,46 @@
 # m03.sh
 
 ## Installing and Configuring containerd
+
+###0 - Install packages
+# containerd prerequisites, first load two modules and configure them to load on boot
+# https://kubernetes.io.docs/setup/production-environment/container-runtimes/
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+# Setup required sysctl params, these persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# Apply sysctl params without reboot
+sudo sysctl --system
+
+# Install containerd
+sudo apt-get update
+sudo apt-get install -y containerd
+
+# Create a containerd configuration file
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+# Set the cgroup driver for containerd to systemd which is required for the kubelet
+# For more information on this config file see:
+# https://github.com/containerd/cri/blob/master/docs/config.md and also
+# https://github.com/containerd/containerd/blob/master/docs/ops.md
+
+# Change SystemdCgroup = false to SystemdCgroup = true
+sudo sed -i 's/            SystemdCgroup = false/            SystemdCgroup = true/' /etc/containerd/config.toml
+
+# Verify the change was ade
+sudo vi /etc/containerd/config.toml
+
+# Restart containerd with the new configuration
+sudo systemctl restart containerd
