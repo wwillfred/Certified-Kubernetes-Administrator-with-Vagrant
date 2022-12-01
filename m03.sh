0ee7a800f40f6d1e969e14ee9b293551e28cc0f8
wwillfred <anicenerd+atlassian@pm.me>
2022-11-30 21:01:41 -0500

    added code for bootstrapping the cluster with kubeadm init

--- m03.sh
+++ m03.sh
@@ -2,7 +2,7 @@
 
 ## Installing and Configuring containerd
 
-###0 - Install packages
+### 0 - Install packages
 # containerd prerequisites, first load two modules and configure them to load on boot
 # https://kubernetes.io.docs/setup/production-environment/container-runtimes/
 sudo modprobe overlay
@@ -67,7 +67,7 @@ sudo apt-mark hold kubelet kubeadm kubectl containerd
 #sudo apt-get install kubelet kubeadm kubectl
 #sudo apt-mark hold kubelet kubeadm kubectl containerd
 
-# 1 - systemd Unites
+### 1 - systemd Unites
 # Check the status of our kubelet and our container runtime, containerd.
 # The kubelet will enter a crashloop until a cluster is created or the node is joined to an existing cluster.
 sudo systemctl status kubelet.service
@@ -76,3 +76,26 @@ sudo systemctl status containerd.service
 # Ensure both are set to start when the system starts up
 sudo systemctl enable kubelet.service
 sudo systemctl enable containerd.service
+
+## Create control plane node - containerd
+
+###IMPORTANT###
+# If you are using containerd, make sure Docker isn't installed.
+# kubeadm init will try to auto-detect the container runtaime and at the moment
+# if both are installed it will pick docker first.
+
+### 0 - Create a cluster
+# Create our kubernetes cluster, specify a pod network range matching that in calico.yaml!
+# Only on the Control Plane Node, download the yaml files for the pod network.
+wget https://docs.projectcalico.org/manifests/calico.yaml
+
+# Look inside calico.yaml and find the setting for Pod Network IP address range CALICO_IPV4POOL_CIDR,
+# adjust if needed for your infrastructure to ensure that the Pod network IP
+# range doesn't overlap with other networks in our infrastructure.
+vi calico.yaml
+
+# You can now just use kubeadm init to bootstrap the cluster
+sudo kubeadm init --kubernetes-version v1.24.3
+# sudo kubeadm init # remove the kubernetes-version parameter if you want to use the latest.
+
+
