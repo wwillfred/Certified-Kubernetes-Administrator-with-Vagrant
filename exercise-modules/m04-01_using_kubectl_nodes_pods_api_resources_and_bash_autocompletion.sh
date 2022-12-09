90635c61768baae9d6c87e606ea3e45df498be88
wwillfred <anicenerd+atlassian@pm.me>
2022-12-09 16:23:52 -0500

    completed script for using kubectl with nodes, pods, etc.

--- exercise-modules/m04-01_using_kubectl_nodes_pods_api_resources_and_bash_autocompletion.sh
+++ exercise-modules/m04-01_using_kubectl_nodes_pods_api_resources_and_bash_autocompletion.sh
@@ -27,3 +27,37 @@ kubectl get pods --namespace kube-system -o wide
 # Now let's get a list of everything that's running in all namespaces
 # In addition to pods, we see services, daemonsets, deployments and replicasets
 kubectl get all --all-namespaces | more
+
+# Asking Kubernetes for the resources it knows about
+# Let's look at the headers in each column. 
+# E.g. StorageClass isn't in a namespace so it's available to all namespaces.
+# Kind is the object type
+kubectl api-resources | more
+
+# no is alias for nodes
+kubectl get no
+
+# We can filter using grep
+kubectl api-resources | grep pod
+
+# Explain an individual resource in detail
+kubectl explain pod | more
+kubectl explain pod.spec | more
+kubectl explain pod.spec.container | more
+kubectl explain pod --recursive | more
+
+# We can take a closer look at our nodes using Describe
+# Check out Name, Taints, Conditions, Addresses, System Info, etc.
+kubectl describe nodes c1-cp1 | more
+kubectl describe nodes c1-node1 | more
+
+# Use -h or --help to find help
+kubectl -h | more
+kubectl get -h | more
+kubectl create -h | more
+
+# Now let's enable bash auto-complete of our kubectl commands
+sudo apt-get install -y bash-completion
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+source ~/.bashrc
+kubectl g[tab][tab] po[tab][tab] --all[tab][tab]
