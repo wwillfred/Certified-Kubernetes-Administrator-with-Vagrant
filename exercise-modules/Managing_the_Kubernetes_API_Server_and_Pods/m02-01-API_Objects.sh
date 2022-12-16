14569c5c715599bc368249d4efda9faab0a7e7b1
wwillfred <anicenerd+atlassian@pm.me>
2022-12-16 08:28:34 -0500

    added code for kubectl dry-run exercises

--- exercise-modules/Managing_the_Kubernetes_API_Server_and_Pods/m02-01-API_Objects.sh
+++ exercise-modules/Managing_the_Kubernetes_API_Server_and_Pods/m02-01-API_Objects.sh
@@ -31,3 +31,38 @@ kubectl get pods
 
 # Remove our pod...this command blocks and can take a second to complete
 kubectl delete pod hello-world
+
+# Working with kubectl dry-run
+# Use kubectl dry-run for server-side validation of a manifest...the object will be sent to the API Server.
+# dry-run=server will tell you the object was created...but it wasn't...
+# it just goes through the whole process but didn't get stored in etcd.
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment.yaml --dry-run=server
+
+# No deployment is created
+kubectl get deployments
+
+# Use kubectl dry-run for client-side validation of a manifest...
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment.yaml --dry-run=client
+
+# Let's do that one more time but with an error... replica should be replicas
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment-error.yaml --dry-run=client
+
+# Use kubectl dry-run client to generate some yaml...for an object
+kubectl create deployment nginx --image=nginx --dry-run=client
+
+# Combine dry-run client with -o yaml and you'll get the YAML for the object...in this case a deployment
+kubectl create deployment nginx --image=nginx --dry-run=client -o yaml | more
+
+# Can be any object...let's try a pod...
+kubectl run pod nginx-pod --image=nginx --dry-run=client -o yaml | more
+
+# We can combine that with IO redirection and store the YAML into a file
+kubectl create deployment nginx --image=nginx --dry-run=client -o yaml > deployment-generated.yaml
+more deployment-generated.yaml
+
+# And then we can deploy from that manifest...or use it as a building block for more complex manifests
+kubectl apply -f deployment-generated.yaml
+
+# Clean up from that demo...you can use delete with -f to delete all the resources in the manifests
+kubectl delete -f deployment-generated.yaml
+
