a1f21283a57cf9e5d15feae36bd9c2de38435659
wwillfred <anicenerd+atlassian@pm.me>
2022-12-17 13:32:06 -0500

    updated driver script for reorgnized vagrant directory

--- exercise-modules/Managing_the_Kubernetes_API_Server_and_Pods/m02-01_API_Objects.sh
+++ exercise-modules/Managing_the_Kubernetes_API_Server_and_Pods/m02-01_API_Objects.sh
@@ -24,7 +24,7 @@ kubectl explain pod.spec | more
 kubectl explain pod.spec.containers | more
 
 # Let's check out some YAML and creating a pod with YAML
-kubectl apply -f /vagrant/pod.yaml
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/02-Using_the_Kubernetes_API/pod.yaml
 
 # Get a list of our currently running pods
 kubectl get pods
@@ -36,16 +36,16 @@ kubectl delete pod hello-world
 # Use kubectl dry-run for server-side validation of a manifest...the object will be sent to the API Server.
 # dry-run=server will tell you the object was created...but it wasn't...
 # it just goes through the whole process but didn't get stored in etcd.
-kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment.yaml --dry-run=server
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/02-Using_the_Kubernetes_API/deployment.yaml --dry-run=server
 
 # No deployment is created
 kubectl get deployments
 
 # Use kubectl dry-run for client-side validation of a manifest...
-kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment.yaml --dry-run=client
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/02-Using_the_Kubernetes_API/deployment.yaml --dry-run=client
 
 # Let's do that one more time but with an error... replica should be replicas
-kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment-error.yaml --dry-run=client
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/02-Using_the_Kubernetes_API/deployment-error.yaml --dry-run=client
 
 # Use kubectl dry-run client to generate some yaml...for an object
 kubectl create deployment nginx --image=nginx --dry-run=client
@@ -69,10 +69,10 @@ kubectl delete -f deployment-generated.yaml
 
 # Working with kubectl diff
 # Create a deployment with 4 replicas
-kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment.yaml
+kubectl apply -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/02-Using_the_Kubernetes_API/deployment.yaml
 
 # Diff that with a deployment with 5 replicas and a new container image...you will see other metadata about the object output too.
-kubectl diff -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/deployment-new.yaml | more
+kubectl diff -f /vagrant/Managing_the_Kubernetes_API_Server_and_Pods/02-Using_the_Kubernetes_API/deployment-new.yaml | more
 
 # Clean up from this demo...you can use delete with -f to delete all the resources in the manifests
 kubectl delete -f deployment.yaml
