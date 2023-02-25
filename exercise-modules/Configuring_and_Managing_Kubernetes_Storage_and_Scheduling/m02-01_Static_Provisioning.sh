db30ee1d098aa159657a884a67b6f2412483628e
wwillfred <anicenerd+atlassian@pm.me>
2023-02-25 12:59:05 -0500

    Added code for configuring persistent volume and persistent volume claim

--- exercise-modules/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/m02-01_Static_Provisioning.sh
+++ exercise-modules/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/m02-01_Static_Provisioning.sh
@@ -18,5 +18,58 @@ cat /etc/exports
 sudo systemctl restart nfs-kernel-server.service
 exit
 
+# On each Node in the cluster...install the NFS client.
+sudo apt install nfs-common -y
+
+# On one of the Nodes, test out basic NFS access before moving on.
+vagrant ssh c1-node1
+sudo mount -t nfs4 c1-storage:/export/volumes /mnt/
+mount | grep nfs
+sudo umount /mnt
+exit
+
+
+# Demo 1 - Static provisioning persistent volumes
 vagrant ssh c1-cp1
 cd /vagrant/declarative-config-files/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/02_Configuring_and_Managing_Storage_in_Kubernetes
+
+# Create a PV with the read/write many and retain as the reclaim policy
+kubectl apply -f nfs.pv.yaml
+
+# Review the created resources, Statuses, Access Mode and Reclaim policy is set to
+# Retain rather than Delete.
+kubectl get PersistentVolume pv-nfs-data
+
+# Look more closely at the PV and its configuration
+kubectl describe PersistentVolume pv-nfs-data
+
+# Create a PVC on that PV
+kubectl apply -f nfs.pvc.yaml
+
+# Check the status, now it's Bound due to the PVC on the PV. See the claim...
+kubectl get PersistentVolume
+
+# Check the status, Bound.
+# We defined the PVC it statically provisioned the PV...but it's not mounted yet.
+kubectl get PersistentVolumeClaim pvc-nfs-data
+kubectl describe PersistentVolumeClaim pvc-nfs-data
+
+# Let's create some content on our storage server
+exit
+vagrant ssh c1-storage
+sudo bash -c 'echo "Hello from our NFS mount!!!" > /export/volumes/pod/demo.html'
+more /export/volumes/pod/demo.html
+exit
+
+
+# Let's create a Pod (in a Deployment and add a Service) with a PVC on pvc-nfs-data
+vagrant ssh c1-cp1
+kubectl apply -f nfs.nginx.yaml
+kubectl get service nginx-nfs-service
+SERVICEIP=$(kubectl get service | grep nginx-nfs-service | awk '{ print $3 }')
+
+# Check to see if our pods are Running before proceeding
+kubectl get pods
+
+# Let's access that application to see our application data...
+curl http://$SERVICEIP/web-app/demo.html
