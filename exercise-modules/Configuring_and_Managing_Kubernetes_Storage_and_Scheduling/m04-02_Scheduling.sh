384223c2e0e978ccfa117e6d760a17c169e066cd
wwillfred <93595582+wwillfred@users.noreply.github.com>
2023-03-24 16:44:40 -0400

    Began driver script for managing Scheduling

--- exercise-modules/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/m04-02_Scheduling.sh
+++ exercise-modules/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/m04-02_Scheduling.sh
@@ -3,4 +3,12 @@
 vagrant ssh c1-cp1
 cd /vagrant/declarative-config-files/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/04_Managing_and_Controlling_the_Kubernetes_Scheduler
 
-# Demo 1 - 
+# Demo - Using Labels to Schedule Pods to Nodes
+# The code is below experiment with on your own.
+#  Course: Managing the Kubernetes API Server and Pods
+#  Module: Managing Objects with Labels, Annotations, and Namespaces
+#  Clip:   Demo: Services, Labels, Selectors, and Scheduling Pods to Nodes
+
+
+# Demo 1a - Using Affinity and Anti-Affinity to schedule Pods to Nodes
+# Let's start off with a deployment of web and cache pods
