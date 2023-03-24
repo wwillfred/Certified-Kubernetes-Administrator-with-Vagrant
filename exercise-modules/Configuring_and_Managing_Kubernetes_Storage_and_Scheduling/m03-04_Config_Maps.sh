ec99d966ceb645f69a88546741190c3e95a64fb7
wwillfred <93595582+wwillfred@users.noreply.github.com>
2023-03-24 16:09:58 -0400

    Completed driver script for ConfigMaps

--- exercise-modules/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/m03-04_Config_Maps.sh
+++ exercise-modules/Configuring_and_Managing_Kubernetes_Storage_and_Scheduling/m03-04_Config_Maps.sh
@@ -34,3 +34,73 @@ echo $PODNAME
 kubectl exec -it $PODNAME -- /bin/sh
 printenv | sort
 exit
+
+# Second as files
+kubectl apply -f deployment-configmaps-files-qa.yaml
+
+# Let's see our configmap exposed as a file using the key as the file name.
+PODNAME=$(kubectl get pods | grep hello-world-configmaps-files-qa | awk '{print $1}' | head -n 1)
+echo $PODNAME
+
+kubectl exec -it $PODNAME -- /bin/sh
+ls /etc/appconfig
+cat /etc/appconfig/appconfigqa
+exit
+
+# Our ConfigMap key was the filename we read in, and the values are inside the file.
+# This is how we can read in whole files at a time and present them to the file
+#   system with the same name in one ConfigMap
+# So think about using this for daemon configs like nginx, redis...etc.
+kubectl get configmap appconfigqa -o yaml
+
+# Updating a ConfigMap, change BACKEND_SERVERNAME to beqa1.example.local
+kubectl edit configmap appconfigqa
+
+kubectl exec -it $PODNAME -- /bin/sh
+watch cat /etc/appconfig/appconfigqa # watch doesn't exist on nginx container
+exit
+
+# Cleaning up our demo
+kubectl delete deployment hello-world-configmaps-env-prod
+kubectl delete deployment hello-world-configmaps-files-qa
+kubectl delete configmap appconfigprod
+kubectl delete configmap appconfigqa
+
+
+# Additional examples of using secrets in your Pods
+# I'll leave this up to you to work with...
+
+#0 - Reading from a directory, each file's basename will be a key in the
+#   ConfigMap...but you can define a key if needed
+kubectl create configmap httpdconfigprod1 --from-file=./configs/
+
+kubectl apply -f deployment-configmaps-directory-qa.yaml
+PODNAME=$(kubectl get pods | grep hello-world-configmaps-directory-qa | awk '{print $1}' | head -n 1)
+
+echo $PODNAME
+
+kubectl exec -it $PODNAME --/bin/sh
+ls /etc/httpd
+cat /etc/httpd/httpd.conf
+cat /etc/httpd/ssl.conf
+exit
+
+#1. Defining a custom key for a file. All configuration will be under that key in 
+#   the filesystem.
+kubectl create configmap appconfigprod1 --from-file=app1=appconfigprod
+kubectl describe configmap appconfigprod1
+kubectl apply -f deployment-configmaps-files-key-qa.yaml
+PODNAME=$(kubectl get pods | grep hello-world-configmaps-files-key-qa | awk '{print $1}' | head -n 1)
+echo $PODNAME
+
+kubectl exec -it $PODNAME -- /bin/sh
+ls /etc/appconfig
+ls /etc/appconfig/app1
+cat /etc/appconfig/app1
+exit
+
+# Clean up after our demos
+kubectl delete deployments hello-world-configmaps-files-key-qa
+kubectl delete deployments hello-world-configmaps-directory-qa
+kubectl delete configmap httpconfigprod1
+kubectl delete configmap appconfigprod1
