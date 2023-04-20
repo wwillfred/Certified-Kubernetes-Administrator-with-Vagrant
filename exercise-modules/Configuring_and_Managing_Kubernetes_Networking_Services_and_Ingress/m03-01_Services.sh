29264999f75524b4a108654ab1ea3ff791bf1f63
wwillfred <93595582+wwillfred@users.noreply.github.com>
2023-04-20 17:12:36 -0400

    Completed code for managing a LoadBalancer Service

--- exercise-modules/Configuring_and_Managing_Kubernetes_Networking_Services_and_Ingress/m03-01_Services.sh
+++ exercise-modules/Configuring_and_Managing_Kubernetes_Networking_Services_and_Ingress/m03-01_Services.sh
@@ -96,3 +96,66 @@ curl http://$CLUSTERIP:$PORT
 # Let's delete that Service
 kubectl delete service hello-world-nodeport
 kubectl delete deployment hello-world-nodeport
+
+
+# 3 - Creating LoadBalancer Services in Azure or any cloud
+# Switch contexts into AKS, we created this cluster together in 'Kubernetes Installation
+#   and Configuration Fundamentals'
+# I've added a script to create a GKE and AKS cluster in this course's downloads
+
+**************
+az login
+# az account set --subscription "{SUBSCRIPTION}"
+
+az group create --name "Kubernetes-Cloud" --location centralus
+
+az aks get-versions --location centralus -o table
+
+# Let's create our AKS managed cluster.
+az aks create \
+    --resource-group "Kubernetes-Cloud" \
+    --generate-ssh-keys \
+    --name CSCluster \
+    --node-count 3
+
+az aks get-credentials --resource-group "Kubernetes-Cloud" --name CSCluster
+
+kubectl config get-contexts
+
+kubectl config use-context 'CSCluster'
+
+kubectl get nodes
+
+***************
+
+# Let's create a Deployment
+kubectl create deployment hello-world-loadbalancer \
+    --image=psk8s.azurecr.io/hello-app:1.0
+
+# Create a LoadBalancer Service
+kubectl expose deployment hello-world-loadbalancer \
+    --port=80 --target-port=8080 --type LoadBalancer
+
+# Can take a minute for the LoadBalancer to provision and get a public IP, you'll see
+#   EXTERNAL-IP as <pending>
+kubectl get service --watch
+
+LOADBALANCERIP=$(kubectl get service hello-world-loadbalancer -o jsonpath='{ .status.loadBalancer.ingress[].ip }')
+curl http://$LOADBALANCERIP:$PORT
+
+# The LoadBalancer, which is "outside" your cluster, sends traffic to the NodePort
+#   Service, which sends it to the ClusterIP to get to your Pods
+# Your cloud LoadBalancer will have health probes checking the health of the NodePort
+#   service on the real Node IPs
+# This isn't the health of our application, that still needs to be configured via
+#   readiness/liveness probes and maintained by your Deployment configuration
+kubectl get service hello-world-loadbalancer
+
+# Clean up the resources from this demo
+kubectl delete deployment hello-world-loadbalancer
+kubectl delete service hello-world-loadbalancer
+
+az group delete --name Kubernetes-Cloud 
+
+# Let's switch back to our local cluster
+kubectl config use-context kubernetes-admin@kubernetes
