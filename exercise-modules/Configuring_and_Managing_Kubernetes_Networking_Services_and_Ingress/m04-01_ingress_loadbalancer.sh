5750f29708748881249645a374b29d96c194c4ae
wwillfred <93595582+wwillfred@users.noreply.github.com>
2023-04-25 14:17:36 -0400

    Completed code for creating an Ingress for a single Service

--- exercise-modules/Configuring_and_Managing_Kubernetes_Networking_Services_and_Ingress/m04-01_ingress_loadbalancer.sh
+++ exercise-modules/Configuring_and_Managing_Kubernetes_Networking_Services_and_Ingress/m04-01_ingress_loadbalancer.sh
@@ -48,3 +48,36 @@ kubectl get services --namespace ingress-nginx
 #   Ingresses we will need to specify an ingressclassname
 kubectl describe ingressclasses nginx
 # kubectl annotate ingressclass nginx "ingressclass.kubernetes.io/si-default-class=true"
+
+
+# Demo 2 - Single Service
+# Create a Deployment, scale it to 2 replicas and expose it as a Service.
+# This Service will be ClusterIP and we'll expose this Service via the Ingress.
+kubectl create deployment hello-world-service-single --image=psk8s.azurecr.io/hello-app:1.0
+kubectl scale deployment hello-world-service-single --replicas=2
+kubectl expose deployment hello-world-service-single --port=80 --target-port=8080 --type=ClusterIP
+
+# Create a single Ingress routing to the one backend service on the Service port 80
+#   listening on all hostnames
+kubectl apply -f ingress-single.yaml
+
+# Get the status of the Ingress. It's routing for all host names on that public IP on port
+#   80
+# This IP will be the same as the EXTERNAL-IP of the Ingress controller...will take a
+#   second to update
+# If you don't define an ingressclassname and don't have a default ingress class the
+#   address won't be updated.
+kubectl get ingress --watch
+kubectl get services --namespace ingress-nginx
+
+# Notice the backends are the Service's Endpoints...so the traffic is going straight from
+#   the Ingress Controller to the Pod, cutting out the kube-proxy hop.
+# Also notice, the default backend is the same Service, that's because we didn't define
+#   any rules, and we just populated ingress.spec.backend. We're going to look at rules
+#   next...
+kubectl describe ingress ingress-single
+
+# Access the application via the exposed ingress on the public IP
+INGRESSIP=$(kubectl get ingress -o jsonpath='{ .items[].status.loadBalancer.ingress[].ip }')
+curl http://$INGRESSIP
+
