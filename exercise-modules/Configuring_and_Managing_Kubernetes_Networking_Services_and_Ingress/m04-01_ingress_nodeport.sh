d61c8e3cfaf5c2300e110babd02904613b1395d2
wwillfred <93595582+wwillfred@users.noreply.github.com>
2023-04-26 11:22:54 -0400

    Revert "Began code for creating and managing NodePort Ingresses" because I don't intend to continue working on it
    This reverts commit 0f4754f6acdceedff51a7b7a23d4c53b021e882a.

--- exercise-modules/Configuring_and_Managing_Kubernetes_Networking_Services_and_Ingress/m04-01_ingress_nodeport.sh
+++ exercise-modules/Configuring_and_Managing_Kubernetes_Networking_Services_and_Ingress/m04-01_ingress_nodeport.sh
@@ -2,11 +2,3 @@ m04-01 ingress nodeport
 
 vagrant ssh c1-cp1
 cd /vagrant/declarative-config-files/Configuring_and_Managing_Kubernetes_Networking_Services_and_Ingress/04_Configuring_and_Managing_Application_Access_with_Ingress
-
-#Check out 1-ingress-loadbalancer.sh for the cloud demos
-
-#Demo 1 - Deploying an ingress controller
-#For our Ingress Controller, we're going to go with nginx, widely available and easy to use.
-#Follow this link here to find a manifest for nginx Ingress Controller for various infrastructures, Cloud, Bare Metal, EKS and more.
-#We have to choose a platform to deploy in...we can choose Cloud, Bare-metal (which we can use in our local cluster) and more.
-https://kubernetes.github.io/ingress-nginx/deploy/
