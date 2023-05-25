d3a25052263a6655e3053e91e77c50972b6c57be
wwillfred <93595582+wwillfred@users.noreply.github.com>
2023-05-25 16:32:43 -0400

    Fixed bug in disable-swap shell script

--- vagrant/disable-swap.sh
+++ vagrant/disable-swap.sh
@@ -5,7 +5,10 @@
 echo "Disabling swap for current session" # not sure if this is necessary
 sudo swapoff -a
 
-echo "Commenting out swap lines in fstab"
-sudo sed -i '/ swap / s/^/#/' /etc/fstab
+echo "Commenting out swap lines in /etc/fstab"
+sudo sed -i.bak -e '/swap/ s/^#*/#/' /etc/fstab
+
+echo "Successfully commented out swap partitions in /etc/fstab:"
+grep swap /etc/fstab
 
 exit 0 
