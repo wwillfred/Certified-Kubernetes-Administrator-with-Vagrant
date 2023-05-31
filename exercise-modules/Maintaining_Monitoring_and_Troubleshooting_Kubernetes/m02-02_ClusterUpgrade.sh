d4aabf8712866913d086a2bacea411ed3945a43a
wwillfred <93595582+wwillfred@users.noreply.github.com>
2023-05-31 15:20:13 -0400

    Completed code for upgrading worker nodes

--- exercise-modules/Maintaining_Monitoring_and_Troubleshooting_Kubernetes/m02-02_ClusterUpgrade.sh
+++ exercise-modules/Maintaining_Monitoring_and_Troubleshooting_Kubernetes/m02-02_ClusterUpgrade.sh
@@ -1,4 +1,4 @@
-m02-02 ClusterUpgrade
+/Users/verabernanski/Documents/Pluralsight/Certified_Kubernetes_Administrator/exercise-modules/Maintaining_Monitoring_and_Troubleshooting_Kubernetes m02-02 ClusterUpgrade
 
 vagrant ssh c1-cp1
 
@@ -63,3 +63,46 @@ kubectl version
 kubectl get nodes
 
 # Upgrade any additional Control Plane Nodes with the same process.
+
+
+# Upgrade the workers, drain the Node, then log into it.
+# Update the environment variable so you can reuse this code over and over.
+NODE=node1
+
+kubectl drain c1-$NODE --ignore-daemonsets
+exit
+vagrant ssh c1-node[XX]
+
+# First, upgrade kubeadm
+sudo apt-mark unhold kubeadm
+sudo apt-get update
+sudo apt-get install -y kubeadm=1.27.2-00
+sudo apt-mark hold kubeadm
+
+# Updates kubelet configuration for the Node
+sudo kubeadm upgrade node
+
+# Update the kubelet and kubectl on the Node
+sudo apt-mark unhold kubelet kubectl
+sudo apt-get update
+sudo apt-get install -y kubelet=1.27.2-00 kubectl=1.27.2-00
+sudo apt-mark hold kubelet kubectl
+
+# Log out of the Node
+exit
+vagrant ssh c1-cp1
+
+# Get the Nodes to show the version...can take a second to update
+kubectl get nodes
+
+# Uncordon the Node to allow workload again
+kubectl uncordon c1-node[XX]
+
+# Check the versions of the Nodes
+kubectl get nodes
+
+####TO DO###
+####BE SURE TO UPGRADE THE REMAINING WORKER NODES#####
+
+# Check the versions of the Nodes
+kubectl get nodes
